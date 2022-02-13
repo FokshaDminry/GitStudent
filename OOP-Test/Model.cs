3def89d52e88fb85216d65c952e77dac90f6b018
FokshaDminry <99391531+FokshaDminry@users.noreply.github.com>
2022-02-13 11:42:33 +0200

    Add project files.

---
+++ OOP-Test/Model.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace OOP_Test
+{
+    class Model : Vehicle
+    {
+        public string _ModelName { get; set; }
+        public int _Weight { get; set; }
+        public DateTime _DateOfMan { get; set; }
+
+        public Model(string Name, int Weight, DateTime DateOfMan, string NameV, double MaxSpeed, int MaxPassengers, int MaxLoadCapacity)
+            : base(NameV, MaxSpeed, MaxPassengers, MaxLoadCapacity)
+        {
+            _ModelName = Name;
+            _Weight = Weight;
+            _DateOfMan = DateOfMan; 
+        }
+
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine($"Model Name: {_ModelName}");
+            Console.WriteLine($"Weight: {_Weight}");
+            Console.WriteLine($"Date Of Manufacture: {_ModelName}");
+            
+        }
+
+        public override void Save()
+        {
+            using(StreamWriter stream = file.AppendText())
+            {
+                stream.WriteLine($"Model: ");
+                stream.WriteLine($"Model Name: {_ModelName}");
+                stream.WriteLine($"Weight: {_Weight}");
+                stream.WriteLine($"Date Of Manufacture: {_DateOfMan}");
+                stream.WriteLine($"---------------------------");
+            }
+            base.Save();
+
+        }
+    }
+}
