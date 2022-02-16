2836651e295d1d535721024a6d698d76c520bd9b
Foksha Dmytry <99391531+FokshaDmitry@users.noreply.github.com>
2022-02-16 15:34:09 +0200

    Add files via upload

---
+++ OOP/OOP/Migrations/202202112104140_OnModel.cs
@@ -0,0 +1,24 @@
+﻿namespace OOP.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class OnModel : DbMigration
+    {
+        public override void Up()
+        {
+            DropPrimaryKey("dbo.Vehicles");
+            AlterColumn("dbo.Vehicles", "ID", c => c.Guid());
+            AlterColumn("dbo.Vehicles", "Id", c => c.Guid(nullable: false));
+            AddPrimaryKey("dbo.Vehicles", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropPrimaryKey("dbo.Vehicles");
+            AlterColumn("dbo.Vehicles", "Id", c => c.Guid());
+            AlterColumn("dbo.Vehicles", "ID", c => c.Guid(nullable: false));
+            AddPrimaryKey("dbo.Vehicles", "Id");
+        }
+    }
+}
